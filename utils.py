d7b4673b65bc5126f6833d4547025f1f66fc5a5a
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-04-30 14:16:24 -0500

    Primer commit

--- utils.py
+++ utils.py
@@ -1,1 +1,25 @@
-# utils.py
+from person import Person
+from bank_account import BankAccount
+
+def person_data():
+    name = input("Enter the person's name:\n")
+    person = Person(name)
+
+    while True:
+        acc_number = int(input("Enter a 4-digit account number:\n"))
+        balance = float(input("Enter the initial balance:\n"))
+
+        account = BankAccount(acc_number, balance)
+        person.add_account(account)
+
+        done = input("Are you done adding accounts? (yes/no):\n")
+        if done == "yes":
+            break
+
+    return person
+
+
+def balance_summary(person_list):
+    for person in person_list:
+        total = sum(account.balance for account in person.accounts)
+        print(f"{person.name} : {total:.2f}")
