a657126e60187097000a85b19805031cce81b3a1
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-04-30 14:12:27 -0500

    Primer commit

--- person.py
+++ person.py
@@ -1,1 +1,10 @@
-# person.py
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.accounts = []
+
+    def add_account(self, account):
+        self.accounts.append(account)
+
+    def __str__(self):
+        return f"Name = {self.name}, Number of accounts = {len(self.accounts)}"
