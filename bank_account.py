234523007a94a691d680b79a335ea7441d948cc9
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-04-30 14:07:28 -0500

    Primer commit

--- bank_account.py
+++ bank_account.py
@@ -1,1 +1,18 @@
-# bank_account.py
+class BankAccount:
+    def __init__(self, account_number, balance=0.0):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return -1
+        else:
+            self.balance -= amount
+            return 0
+
+    def __str__(self):
+        acc_str = str(self.account_number)
+        return f"Account Number: **{acc_str[-2:]}\nCurrent Balance: {self.balance:.2f}"
