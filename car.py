25e5a1de62dbfd2ea51aafecd57ac67106137e51
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-04-30 13:52:39 -0500

    Primer commit

--- car.py
+++ car.py
@@ -1,2 +1,17 @@
-# Write your class definition here!
+class Car:
+    def __init__(self, car_id, brand, year, color, mileage=0.0):
+        self.car_id = car_id
+        self.brand = brand
+        self.year = year
+        self.color = color
+        self.mileage = mileage
+
+    def change_color(self, new_color):
+        self.color = new_color
+
+    def drive(self, miles):
+        self.mileage += miles
+
+    def __str__(self):
+        return f"{self.car_id} - {self.year} {self.color} {self.brand} with {self.mileage} miles"
 
