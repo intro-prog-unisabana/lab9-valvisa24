b360e726978639ae1ccba66a7b14fac843331716
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-04-30 13:26:21 -0500

    Primer commit

--- song.py
+++ song.py
@@ -1,1 +1,8 @@
-# Write your code here!
+class Song:
+    def __init__(self, name, artist, length):
+        self.name = name
+        self.artist = artist
+        self.length = length
+
+    def get_length_in_seconds(self):
+        return self.length * 60
